00b8f0e2681ecf891b21a00b4c5e17701a0f5d6f
bhaskarbala975 <bhaskarbala975@missionrnd.com>
2016-03-10 21:47:37 +0530

    Submit Commit

--- src/numberToLinkedList.cpp
+++ src/numberToLinkedList.cpp
@@ -13,12 +13,62 @@ NOTES: For negative numbers ignore negative sign.
 
 #include <stdio.h>
 #include <malloc.h>
+#include<stdlib.h>
 
 struct node {
 	int num;
 	struct node *next;
 };
+struct node* insertfront(struct node*, int);
+struct node* insert(struct node*, int);
+struct node * numberToLinkedList(int N) 
+{
+	int a[100], temp = 0,i=0,j=0;
+	struct node* first;
+	first = NULL;
+	if (N < 0)
+	{
+		N = N*-1;
+	}
+	if (N == 0)
+	{
+		first = insert(first, N);
+		return first;
+	}
+	if (N > 0)
+	{
+		while (N != 0)
+		{
+			temp = N % 10;
+			a[i] = temp;
+			N = N / 10;
+			i++;
+		}
 
-struct node * numberToLinkedList(int N) {
-	return NULL;
+		for (j = 0; j < i; j++)
+		{
+			first = insertfront(first, a[j]);
+		}
+
+		return first;
+	}
+}
+struct node* insertfront(struct node* first, int i)
+{
+	struct node* p;
+	p = (struct node*)malloc(sizeof(struct node));
+	p->num = i;
+	p->next = first;
+	first = p;
+	return first;
+}
+struct node* insert(struct node* first, int i)
+{
+	struct node* p;
+	p = (struct node*)malloc(sizeof(struct node));
+	p->num = i;
+	p->next = first;
+	first = p;
+	return first;
+
 }
