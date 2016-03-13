774b937b485d0b8c847f7d7a46679dd6a1d6fa36
bhaskarbala975 <bhaskarbala975@missionrnd.com>
2016-03-13 11:12:19 +0530

    Submit Commit

--- src/removeEveryKthNode.cpp
+++ src/removeEveryKthNode.cpp
@@ -12,6 +12,8 @@ NOTES:
 */
 
 #include <stdio.h>
+#include<malloc.h>
+#include<stdlib.h>
 
 struct node {
 	int num;
@@ -20,23 +22,49 @@ struct node {
 
 struct node * removeEveryKthNode(struct node *head, int K) 
 {
-	int count = 0;
+	int totcount = 0, temp = 0, i = 0, c = 0;
 	struct node* t;
-	while (t != NULL)
-	{
-		count++;
-		t = t->next;
-
-	}
-
-	if (K<=1)
-	return NULL;
+	struct node* pre;
+	struct node* t1;
+	if (K <= 1)
+		return NULL;
 	else if (head == NULL)
 		return NULL;
-	
 	else
 	{
 		t = head;
-		
+		temp = K;
+		t1 = head;
+		if (K > totcount)
+			return head;
+		if (K < totcount)
+		{
+			for (i = 0; i < totcount; i++)
+			{
+				if (temp == i&&temp < totcount)
+				{
+					while (t1 != NULL&&c == K)
+					{
+						pre = t1;
+						t1 = t1->next;
+					}
+					pre->next = t->next;
+					free(t);
+					t = t->next;
+					temp = temp + K;
+				}
+				else if (K < totcount)
+					t = t->next;
+				else
+					break;
+			}
+
+		}
+		return head;
 	}
+
+	
+	
+	
+
 }
--- src/sortLinkedList.cpp
+++ src/sortLinkedList.cpp
@@ -12,12 +12,49 @@ NOTES: Without using extra array or linked list.
 */
 
 #include <stdio.h>
+int count(struct node* head);
 
 struct node {
 	int num;
 	struct node *next;
 };
 
-struct node * sortLinkedList(struct node *head) {
+struct node * sortLinkedList(struct node *head)
+{
+	int n = 0, i = 0, t;
+	struct node* t1;
+	struct node* t2;
+	if (head==NULL)
 	return NULL;
+	n = count(head);
+	if (n == 1)
+		return head;
+	else
+	{
+		while (i <= (n - 1))
+		{
+			t1 = head;
+			t2 = head->next;
+			while (t2 != NULL)
+			{
+				if ((t1->num) > (t2->num))
+				{
+					t = t1->num;
+					t1->num = t2->num;
+					t2->num = t;
+				}
+				t1 = t1->next;
+				t2 = t2->next;
+			}
+			i++;
+		}
+		return head;
+	}
+}
+int count(struct node* head)
+{
+	if (head == NULL)
+		return 0;
+	else
+		return (1 + count(head->next));
 }
